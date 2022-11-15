1835746ecabc7484dc376415c0b0f2a0d2abc728
Stern_Ritter <fadeef.k@yandex.ru>
2022-11-16 00:05:28 +0300

    Edit cofig file

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -15,7 +15,6 @@ module.exports = {
   apps: [
     {
       name: "frontend",
-      script: "./build/index.js",
     },
   ],
 
@@ -28,7 +27,7 @@ module.exports = {
       path: DEPLOY_PATH,
       ssh_options: "StrictHostKeyChecking=no",
       "post-deploy":
-        "cd frontend && npm i && npm run build && pm2 start ecosystem.config.js && pm2 save",
+        "cd frontend && npm i && npm run build",
     },
   },
 };
