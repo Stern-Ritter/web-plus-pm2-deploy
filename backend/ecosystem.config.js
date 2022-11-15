01d2e85fd2d59e668e3e2001ba2fda382a622f8a
Stern_Ritter <fadeef.k@yandex.ru>
2022-11-15 22:40:47 +0300

    Edit config file

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
       ssh_options: "StrictHostKeyChecking=no",
       "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       "post-deploy":
-        "cd backend && npm i && npm run build && pm2 start /dist/app.js",
+        "cd backend && npm i && npm run build && pm2 start ./dist/app.js",
     },
   },
 };
